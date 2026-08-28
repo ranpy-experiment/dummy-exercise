56cd1bd0916720065be84f701e0e936fed19cac6
ranpy13 <85052085+ranpy13@users.noreply.github.com>
2026-08-29 00:50:33 +0530

    sovlve(leet-80): remove duplicate from sorted array ii

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,9 +1,15 @@
 pub mod collatz_sequence;
 pub mod maximum_twin_sum_of_list_node;
+pub mod remove_duplicate_from_sroted_array;
 
 pub use collatz_sequence::CollatzSequence;
 pub use maximum_twin_sum_of_list_node::MaximumTwinSum;
+pub use remove_duplicate_from_sroted_array::RemoveDuplicateTwo;
 
 pub fn all_solutions() -> Vec<Box<dyn crate::solution::Solution>> {
-    vec![Box::new(MaximumTwinSum {}), Box::new(CollatzSequence {})]
+    return vec![
+        Box::new(MaximumTwinSum {}),
+        Box::new(CollatzSequence {}),
+        Box::new(RemoveDuplicateTwo {}),
+    ];
 }
