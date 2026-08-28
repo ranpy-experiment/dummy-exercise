e0b0fe8ea684f616610b91ac052cf0f4bdc159cf
Randeep Piyush <85052085+ranpy13@users.noreply.github.com>
2026-08-29 00:47:17 +0530

    solve(leet-2130): maximum twin sum of a linked list (#5)

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let idx: usize = std::env::args()
         .nth(1)
         .and_then(|s| s.parse().ok())
-        .unwrap_or(0);
+        .unwrap_or(all.len() - 1);
     let s = &all[idx];
     println!("Running: {}", s.name());
     utils::get_downcast_string_result(s.solve());
